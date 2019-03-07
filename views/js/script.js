9531597c73ca3b2053ce9cf29d0b1680790af043
Sargis <sargisbelaryan1989@gmail.com>
2019-03-07 17:47:53 +0400

    Remove no needed styles.

--- views/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#image").on('change', function(){
-    	//$('.imageLabel').text('Downloaded '+ $(this).val());
-		$('.imageLabel').text($(this).val().replace(/C:\\fakepath\\/i, ''));
-    });
-});
